c4018a65f84ec688fd10158bbe5bb06edc6af69d
Ethan <baueyethan3012@gmail.com>
2023-04-21 21:35:19 -0400

    floating point testing

---
+++ Day-1/LearningMaterial/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a = 0.1;
+    float b = 0.2;
+    float c = 0.1 + 0.2;
+    cout << a + b << endl;
+    cout << c;
+    return 0;
+}
